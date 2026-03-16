3eef5941345c2574e699bb324e3a743b04f4a8be
Kimulegen <kimulegen@proton.me>
2026-03-15 21:08:21 -0300

    feat: solve problem 038

--- src/015-essential-types-and-annotations/038-type-async-functions.problem.ts
+++ src/015-essential-types-and-annotations/038-type-async-functions.problem.ts
@@ -1,10 +1,19 @@
 import { Expect, Equal } from "@total-typescript/helpers";
 
-async function fetchData() {
+/* Solution 0 */
+// async function fetchData() {
+//   const response = await fetch("https://api.example.com/data");
+//   const data = await response.json() as number;
+//   return data;
+// }
+
+/* Solution 2 */
+async function fetchData(): Promise<number> {
   const response = await fetch("https://api.example.com/data");
   const data = await response.json();
   return data;
 }
+
 
 const example = async () => {
   const data = await fetchData();
