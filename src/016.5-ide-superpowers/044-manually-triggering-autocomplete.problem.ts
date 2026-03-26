6196573a48e0a2638a603338abafa1d869e1ecc9
Kimulegen <kimulegen@proton.me>
2026-03-26 13:22:22 -0300

    feat: solve problem 044

--- src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
+++ src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
@@ -8,11 +8,14 @@ const acceptsObj = (obj: MyObj) => {};
 
 acceptsObj({
   // Autocomplete in here!
+  foo: 'string',
+  bar: 1,
+  baz: true
 });
 
 document.addEventListener(
   // Autocomplete this string!
-  "",
+  "DOMContentLoaded",
   (event) => {
     console.log(event);
   },
