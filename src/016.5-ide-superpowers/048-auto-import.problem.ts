0f22b0119cc1f0f9f1fd98ed313b5572d8edec64
Kimulegen <kimulegen@proton.me>
2026-03-26 13:37:09 -0300

    feat: solve problem 048

--- src/016.5-ide-superpowers/048-auto-import.problem.ts
+++ src/016.5-ide-superpowers/048-auto-import.problem.ts
@@ -1,6 +1,24 @@
+import { SESSION_EXPIRATION_TIME, SESSION_EXPIRATION_TIME_IN_SECONDS, SESSION_EXPIRATION_TIME_IN_MINUTES, SESSION_EXPIRATION_TIME_IN_HOURS } from "./dummy-import";
+
+/* Soluction 1 */
+// import * as dummy from './dummy-import'
+
+// const expirationTimes = [
+//   dummy.SESSION_EXPIRATION_TIME,
+//   dummy.SESSION_EXPIRATION_TIME_IN_SECONDS,
+//   dummy.SESSION_EXPIRATION_TIME_IN_MINUTES,
+//   dummy.SESSION_EXPIRATION_TIME_IN_HOURS,
+// ];
+
+/* Solution 2 */
+// Use ctrl+space in each variable and use autocomplete to import them
+
+/* Solution 3 */
+// Use ctrl+. to open quick fix and use 'Add all missing imports'
 const expirationTimes = [
-  SESSION_EXPIRATION_TIME,
-  SESSION_EXPIRATION_TIME_IN_SECONDS,
-  SESSION_EXPIRATION_TIME_IN_MINUTES,
-  SESSION_EXPIRATION_TIME_IN_HOURS,
+ SESSION_EXPIRATION_TIME,
+ SESSION_EXPIRATION_TIME_IN_SECONDS,
+ SESSION_EXPIRATION_TIME_IN_MINUTES,
+ SESSION_EXPIRATION_TIME_IN_HOURS,
 ];
+
