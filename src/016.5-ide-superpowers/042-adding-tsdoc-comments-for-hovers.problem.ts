d052d88665343a78e341ebca66651ea0a2f160f0
Kimulegen <kimulegen@proton.me>
2026-03-26 13:09:32 -0300

    feat: solve problem 042

--- src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
+++ src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
@@ -1,3 +1,11 @@
+/** 
+ * Adds two numbers together.
+  * @param a - The first number to add.
+  * @param b - The second number to add.
+  * @returns The sum of the two numbers.
+*/
 const myFunction = (a: number, b: number) => {
   return a + b;
 };
+
+myFunction(1,2)
