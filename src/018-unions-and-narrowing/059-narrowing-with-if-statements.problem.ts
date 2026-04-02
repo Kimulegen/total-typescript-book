f201bbec287f9c9d8192405fb127f9c931b8291e
Kimulegen <kimulegen@proton.me>
2026-04-02 15:11:35 -0300

    feat: solve problem 059

--- src/018-unions-and-narrowing/059-narrowing-with-if-statements.problem.ts
+++ src/018-unions-and-narrowing/059-narrowing-with-if-statements.problem.ts
@@ -1,11 +1,56 @@
 import { expect, it } from "vitest";
 
+/* Solution 1 */
+// function validateUsername(username: string | null): boolean {
+//   // Rewrite this function to make the error go away
+//   if (typeof username === "string") {
+//     return username.length > 5;
+//   }
+
+//   return false;
+// }
+
+/* Solution 2 */
+// function validateUsername(username: string | null): boolean {
+//   // Rewrite this function to make the error go away
+//   if (username) {
+//     return username.length > 5;
+//   }
+
+//   return false;
+// }
+
+/* Solution 3 */
+// function validateUsername(username: string | null): boolean {
+//   // Rewrite this function to make the error go away
+//   if (typeof username !== "string") {
+//     return false;
+//   }
+
+//   return username.length > 5;
+// }
+
+/* Solution 4 */
+// function validateUsername(username: string | null): boolean {
+//   // Rewrite this function to make the error go away
+//   if (typeof username === "object") {
+//     return false;
+//   }
+
+//   return username.length > 5;
+// }
+
+/* Solution 5 */
 function validateUsername(username: string | null): boolean {
+  const isUsernameOK = typeof username === "string"
   // Rewrite this function to make the error go away
-  return username.length > 5;
+  if (isUsernameOK) {
+    return username.length > 5;
+  }
 
   return false;
 }
+
 
 it("should return true for valid usernames", () => {
   expect(validateUsername("Matt1234")).toBe(true);
