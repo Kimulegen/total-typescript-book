ef3b150d4f653c47e7707ea3fde230544c9b1b1f
Kimulegen <kimulegen@proton.me>
2026-04-02 14:56:06 -0300

    feat: solve problem 055

--- src/018-unions-and-narrowing/055-combining-unions.problem.ts
+++ src/018-unions-and-narrowing/055-combining-unions.problem.ts
@@ -1,14 +1,18 @@
 import { Equal, Expect } from "@total-typescript/helpers";
 
-type HttpCode = "400" | "401" | "404" | "500" | "200" | "201" | "204";
+// type HttpCode = "400" | "401" | "404" | "500" | "200" | "201" | "204";
 
-const handleErrorCase = (code: string) => {
+type HttpErrorCode = "400" | "401" | "404" | "500"
+type HttpSuccesCode = "200" | "201" | "204"
+type HttpCode = HttpErrorCode | HttpSuccesCode
+
+const handleErrorCase = (code: HttpErrorCode) => {
   // An imaginary function where we only handle the errors
 
   type test = Expect<Equal<typeof code, "400" | "401" | "404" | "500">>;
 };
 
-const handleSuccessCase = (code: string) => {
+const handleSuccessCase = (code: HttpSuccesCode) => {
   // An imaginary function where we only handle the success cases
 
   type test = Expect<Equal<typeof code, "200" | "201" | "204">>;
